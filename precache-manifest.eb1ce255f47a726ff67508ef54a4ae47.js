9c8dea86bbddc7c7df62a93728f5cd94498c362f
Kevin Anantha <kevin.anantha@gmail.com>
2019-05-05 23:56:30 +0800

    Updates

---
+++ precache-manifest.eb1ce255f47a726ff67508ef54a4ae47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c7fdeefcb387d5e0a55904d0fb978997",
+    "url": "/index.html"
+  },
+  {
+    "revision": "89a71fc9b3790931e58d",
+    "url": "/static/css/main.22be8229.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "89a71fc9b3790931e58d",
+    "url": "/static/js/main.28647029.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
